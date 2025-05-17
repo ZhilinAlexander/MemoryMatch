91e8deb0d3aa8efabee504ef9dbe8b130cd3edb9
AlexanderZhilin <alexsandr.zh94@gmail.com>
2025-05-17 17:29:56 +0300

    Initial Commit

---
+++ MemoryMatchTests/MemoryMatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryMatchTests.swift
+//  MemoryMatchTests
+//
+//  Created by Alexander Zh. on 17.05.25.
+//
+
+import Testing
+@testable import MemoryMatch
+
+struct MemoryMatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
